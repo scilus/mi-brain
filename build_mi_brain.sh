ee2cdc3511933a06091f09dbc5ca8f7ebf604191
MapleGlazedCrow <constant.bastonnais@hotmail.ca>
2026-06-08 10:52:17 -0400

    Scripts needed to use the debugger

---
+++ build_mi_brain.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+cd ~/mi-brain/mi-brain/build-2025
+
+rm -rf *
+
+cmake \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DMITK_DIR=$HOME/Libraries/build/mitk-2025.12-build/MITK-build \
+    -DQt6_DIR=$HOME/Libraries/install/qt-6.8.3/lib/cmake/Qt6 \
+    ..
+
+cmake --build . --parallel
